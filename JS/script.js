6a2ff18d06f6bf3d46a66e7e4f8fa8de04fda526
jorgeduardos <jorgeduardo.1994@gmail.com>
2017-04-21 15:11:20 -0500

    added sidebar quote skeleton

--- JS/script.js
+++ JS/script.js
@@ -28,6 +28,11 @@ $(document).ready(function() {
             backSpeed: 0
 		});
 	});
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
     
     
     /* NAV BAR ANIMATIONS */
