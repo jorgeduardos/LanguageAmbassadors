33c25acd3dcbd11653a4841d6fde822345501bfb
jorgeduardos <jorgeduardo.1994@gmail.com>
2017-05-12 17:42:24 -0500

    fixing php translation form to handle files

--- php/phpMailerForms/translationMailer.php
+++ php/phpMailerForms/translationMailer.php
@@ -1,6 +1,19 @@
 
 <?php
 
+    // Get the form fields, removes html tags and whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone = trim($_POST["number"]);
+    $sub = trim($_POST["subject"]);
+    $source = trim($_POST["source"]);
+    $target = trim($_POST["target"]);
+
+    
+
+    $message = trim($_POST["comments"]);
+
 $msg = '';
 if (array_key_exists('file', $_FILES)) {
     // First handle the upload
@@ -37,4 +50,36 @@ if (array_key_exists('file', $_FILES)) {
     } else {
         $msg .= 'Failed to move file to ' . $uploadfile;
     }
+}else{
+
+    // Check the data.
+    if (empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: /index.php?success=-1#form");
+        exit;
+    }
+
+    // Set the recipient email address. Update this to YOUR desired email address.
+    $recipient = "jorgeduardo.1994@gmail.com";
+
+    // Set the email subject.
+    $subject = "New contact from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Phone Number: $phone\n";
+    $email_content .= "Subject: $sub\n";
+    $email_content .= "Source Language: $source\n";
+    $email_content .= "Target Language: $target\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    mail($recipient, $subject, $email_content, $email_headers);
+    
+    // Redirect to the index.html page with success code
+    header("Location: /index.php?tsuccess=1#translation");
+
 }
